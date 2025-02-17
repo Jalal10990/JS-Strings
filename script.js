8f6dc20e5e9b9db74686369f3a41d11d5a02bcbd
Shah jalal <jalal.chishti122448@gmail.com>
2025-02-17 08:18:37 -0800

    JS  Strings notes

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("This is strings tutorial")
+let a = "Jalal";
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let realName = "Ali";
+let friend = "Sami";
+console.log("His name is " + realName + " and his friend name is " + friend )
+
+// tamplete literal syntex 
+console.log(`His name is ${realName} and his friend name is ${friend}`)
+
+let b = "Pakistan";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 4))
+console.log(b.slice(1))
+
+console.log(b.replace("Pak", "786"))
+console.log(b.concat(a, "DIL"))
+
+let c= "Ahmad"
+console.log(c.charAt(0))
+console.log(c.indexOf("ma"))
+console.log(c.startsWith(Ah))
+console.log(c.startsWith(ma))
+console.log(c.endsWith(ad))
